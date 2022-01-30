381dab3bf04392e6b2e3654a816f83e20beb88f1
Inveron <michaeldensmorem@gmail.com>
2022-01-30 13:57:07 -0500

    Full project

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # RockPaperScissors
 Rps project i coded in javascript per the Odin Projects cirriculumn.
+
+
+I have technically done this project before about like 7-10 months ago now. I spent days on it and in the end it barely ran. This time,
+it was much easier, finished in basically half an hour and it runs a lot better than i thought it would have.
+
---
+++ rps.js
@@ -0,0 +1,71 @@
+let score = 0;
+
+function computerPlay() {
+    let choices = ['rock', 'paper', 'scissors'];
+
+    let randchoice = Math.floor(Math.random() * choices.length)
+
+    return choices[randchoice];
+}
+
+function playRound(playerSelection, computerSelection) {
+    computerSelection = computerPlay();
+    playerSelection = prompt("Whats your move?");
+    
+    playerSelection.toLowerCase();
+
+    if (playerSelection == computerSelection) {
+        alert("Draw!");
+    }
+
+    if (playerSelection != computerSelection) {
+        if (playerSelection == "rock" && computerSelection == "scissors") {
+            alert("Rock beats scissors, you won!");
+            score++;
+        }
+
+        else if (playerSelection == "paper" && computerSelection == "rock") {
+            alert("Paper beats rock, you won!");
+            score++;
+        }
+
+        else if (playerSelection == "scissors" && computerSelection == "paper") {
+            alert("Scissors beats paper, you won!");
+            score++;
+        }
+
+        else {
+            alert("You lost!");
+        }
+    }
+}
+
+function game() {
+    let computerSelection = '';
+    let playerSelection = '';
+
+    alert("Final results are also shown in the console!");
+    
+    playRound(computerSelection, playerSelection);
+    playRound(computerSelection, playerSelection);
+    playRound(computerSelection, playerSelection);
+    playRound(computerSelection, playerSelection);
+    playRound(computerSelection, playerSelection);
+
+    
+
+    if (score >= 3) {
+        console.log("You won the game!!!")
+        alert("You won the game!!!")
+    }
+
+    else {
+        console.log("You lost the game, better luck next time!")
+        alert("You won the game!!!")
+    }
+
+
+}
+
+game();
+
